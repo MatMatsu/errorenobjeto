49f8b4d6f686a8fd31350e55c553f5ef2fbed909
MatMatsu <32300160+MatMatsu@users.noreply.github.com>
2018-10-25 00:17:41 -0300

    Add files via upload

---
+++ src/Principal.java
@@ -0,0 +1,16 @@
+
+public class Principal {
+    
+    public static void main (String args[]){
+        
+        Saiayin s1 = new Saiayin("Goku", "Vegito", 4, 152);
+        
+        /* 
+        s1.nombre="Goku";
+        s1.nivel=4;
+        s1.planeta="Vegito";
+        s1.ki=152;
+        */
+       
+    }
+}
